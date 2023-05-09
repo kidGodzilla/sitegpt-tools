59aeffdcc52f608d337667f0527348c0cd24ba8b
James Futhey <kidGodzilla@users.noreply.github.com>
2023-05-09 15:45:12 -0700

    Update main.js

--- main.js
+++ main.js
@@ -12,21 +12,21 @@ window._sitegpt = {
   hide: function() { document.querySelector('#sitegpt-chat-icon').style.display = 'none' },
   exists: function() { return !!document.querySelector('#sitegpt-chat-icon') },
   
-	on_ready: function (callback) {
-		window.__hide_sitegpt_interval = setInterval(function() {
-			if (_sitegpt.exists()) {
-				clearInterval(window.__hide_sitegpt_interval);
-				if (callback && typeof callback === 'function') callback();
-			}
-		}, 250);
-	},
+  on_ready: function (callback) {
+    window.__hide_sitegpt_interval = setInterval(function() {
+      if (_sitegpt.exists()) {
+        clearInterval(window.__hide_sitegpt_interval);
+        if (callback && typeof callback === 'function') callback();
+      }
+    }, 250);
+  },
   add_to_crisp: function() {
-		var el = document.querySelector('a[data-mode="chat"]');
-		var clone = el.cloneNode(true);
-		clone.classList.add('instant-answers');
-		clone.setAttribute('onclick', 'window._sitegpt.toggle();$crisp.push(["do", "chat:close"]);');
-		el.after(clone);
-		document.querySelectorAll('.instant-answers span')[1].innerText = 'Instant Answers';
-	},
-  
+    if (document.querySelector('.instant-answers')) return;
+    var el = document.querySelector('a[data-mode="chat"]');
+    var clone = el.cloneNode(true);
+    clone.classList.add('instant-answers');
+    clone.setAttribute('onclick', 'window._sitegpt.toggle();$crisp.push(["do", "chat:close"]);');
+    el.after(clone);
+    document.querySelectorAll('.instant-answers span')[1].innerText = 'Instant Answers';
+  },
 }
